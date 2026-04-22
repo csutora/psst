5b816209548c22ce45b4d768598c49786f41ebd9
numinara <numi@nara.sh>
2026-04-22 19:49:46 +0800

    v0.1.2

--- flake.nix
+++ flake.nix
@@ -15,12 +15,12 @@
       forLinux = f: nixpkgs.lib.genAttrs linuxSystems (system: f nixpkgs.legacyPackages.${system});
       forAll = f: nixpkgs.lib.genAttrs allSystems (system: f nixpkgs.legacyPackages.${system});
 
-      version = "0.1.1";
+      version = "0.1.2";
 
       # darwin: fetch pre-built signed binary from github releases
       # update hash after `make dist && gh release create`
       darwinUrl = "https://github.com/csutora/psst/releases/download/v${version}/psst-${version}-aarch64-darwin.tar.gz";
-      darwinHash = "sha256-NU+BVQw7/isfCqWH9LP49p4m7KWkFkfpc33otoEGheg=";
+      darwinHash = "sha256-8IzvKIJotsY4dJzGvGo6pFR+zgR20iQA0MHVxjf3pBE=";
 
       mkDarwin = pkgs: pkgs.stdenv.mkDerivation {
         pname = "psst";
